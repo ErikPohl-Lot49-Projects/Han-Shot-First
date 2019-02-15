9aa093c8ed256c3044d31316668f854710463879
Erik Pohl <iotasubscript@gmail.com>
2019-02-15 14:49:07 -0500

    test cases
    test cases

---
+++ test_viewtree_search.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from viewtree_search import viewtree_search
+from json import load
+
+
+class TestCases(TestCase):
+    
+    def testcase1(self):
+        with open('C:\\Users\\p636205\\workspace\\jnesaisq\\JnesaisQ\\the_modal_nodes.json','r') as f1:
+            z = load(f1)
+        self.assertEqual(viewtree_search(z, ["Input"]), 26)
