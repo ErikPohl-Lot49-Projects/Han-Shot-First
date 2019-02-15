16cb9c7001e502f6b14d2243a01330d44782e50b
Erik Pohl <iotasubscript@gmail.com>
2019-02-15 15:29:49 -0500

    output json return count
    I modified this for cleaner output

--- viewtree_search.py
+++ viewtree_search.py
@@ -29,17 +29,39 @@ def viewtree_search(json_view_tree, search_commands):
                     if command.startswith('#'):
                         logging.info('trying identifier [' + i + ']')
                         if (i=='identifier') and (json_view_tree[i] == command[1:]):
-                            print(25*'#'+i + ' : ' + json_view_tree[i])
+                            #print(25*'#'+i + ' : ' + json_view_tree[i], json_view_tree)
+                            print(json_view_tree)
                             count +=1
                     elif command.startswith('.'):
                         if (i == 'classNames') and (command[1:] in json_view_tree[i]):
                             logging.info(json_view_tree[i])
-                            print(25*'>'+i + ' : ' + ''.join(json_view_tree[i]))
+                            #print(25*'>'+i + ' : ' + ''.join(json_view_tree[i]), json_view_tree)
+                            print(json_view_tree)
                             count +=1
                     elif  (i == 'class') and (json_view_tree[i] == command[0:]):
-                        print(25*'*'+i + ' : ' + json_view_tree[i])
+                        #print(25*'*'+i + ' : ' + json_view_tree[i], json_view_tree)
+                        print(json_view_tree)
                         count +=1
                         
     return count
 
+with open('C:\\Users\\p636205\\workspace\\jnesaisq\\JnesaisQ\\the_modal_nodes.json','r') as f1:
+    z = load(f1)
 
+with open('C:\\Users\\p636205\\workspace\\jnesaisq\\JnesaisQ\\format.json','r') as f1:
+    json_query_format = load(f1)
+mysearch = ['Input']
+output = viewtree_search(z,mysearch )
+print(output)
+mysearch = ['#videoMode']
+output = viewtree_search(z,mysearch )
+print(output)
+mysearch = ['#rate']
+output = viewtree_search(z,mysearch )
+print(output)
+mysearch = ['#supersample']
+output = viewtree_search(z,mysearch )
+print(output)
+mysearch = ['.container']
+output = viewtree_search(z,mysearch )
+print(output)
