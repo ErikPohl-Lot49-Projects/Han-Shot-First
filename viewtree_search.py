6de910f477d2856cdf8602e5132d936ce5f742a4
Erik Pohl <iotasubscript@gmail.com>
2019-02-15 14:50:19 -0500

    removed demo usage and test cases

--- viewtree_search.py
+++ viewtree_search.py
@@ -42,23 +42,4 @@ def viewtree_search(json_view_tree, search_commands):
                         
     return count
 
-with open('C:\\Users\\p636205\\workspace\\jnesaisq\\JnesaisQ\\the_modal_nodes.json','r') as f1:
-    z = load(f1)
 
-with open('C:\\Users\\p636205\\workspace\\jnesaisq\\JnesaisQ\\format.json','r') as f1:
-    json_query_format = load(f1)
-mysearch = ['Input']
-output = viewtree_search(z,mysearch )
-print(output)
-mysearch = ['#videoMode']
-output = viewtree_search(z,mysearch )
-print(output)
-mysearch = ['#rate']
-output = viewtree_search(z,mysearch )
-print(output)
-mysearch = ['#supersample']
-output = viewtree_search(z,mysearch )
-print(output)
-mysearch = ['.container']
-output = viewtree_search(z,mysearch )
-print(output)
