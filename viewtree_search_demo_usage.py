54a2918dcc1fd676e39b1e54898483b4d3fb7a13
Erik Pohl <iotasubscript@gmail.com>
2019-02-16 09:33:50 -0500

    added more complicated cases
    uses halt on match and explores why it was used

--- viewtree_search_demo_usage.py
+++ viewtree_search_demo_usage.py
@@ -1,23 +1,33 @@
 from json import load
-from JnesaisQ.viewtree_search import viewtree_search
+from viewtree_search import viewtree_search
 
-with open('C:\\Users\\p636205\\workspace\\jnesaisq\\JnesaisQ\\the_modal_nodes.json','r') as f1:
+with open('C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json','r') as f1:
     z = load(f1)
 
-with open('C:\\Users\\p636205\\workspace\\jnesaisq\\JnesaisQ\\format.json','r') as f1:
-    json_query_format = load(f1)
+search_hits = [0]
 mysearch = ['Input']
-output = viewtree_search(z,mysearch )
+output = viewtree_search(z,mysearch , search_hits)
 print(output)
 mysearch = ['#videoMode']
-output = viewtree_search(z,mysearch )
+output = viewtree_search(z,mysearch , search_hits )
 print(output)
 mysearch = ['#rate']
-output = viewtree_search(z,mysearch )
+output = viewtree_search(z,mysearch , search_hits )
 print(output)
 mysearch = ['#supersample']
-output = viewtree_search(z,mysearch )
+output = viewtree_search(z,mysearch , search_hits )
 print(output)
 mysearch = ['.container']
-output = viewtree_search(z,mysearch )
+output = viewtree_search(z,mysearch , search_hits, halt_on_match=True )
+print(output)
+
+
+with open('C:\\Users\\Richard Pendrake\\Downloads\\mos_eisley.json','r') as f1:
+    z = load(f1)
+search_hits = [0,0]
+mysearch = ['#System', '.container']
+output = viewtree_search(z,mysearch , search_hits, halt_on_match=True )
+print(output)
+mysearch = ['#System', '.container']
+output = viewtree_search(z,mysearch , search_hits, halt_on_match=False)
 print(output)
