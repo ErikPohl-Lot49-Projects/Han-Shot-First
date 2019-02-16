4b6e588da77a2f71e68108dda3ab3f702d9595b3
Erik Pohl <iotasubscript@gmail.com>
2019-02-16 09:36:10 -0500

    uses halt on match = False
    I am assuming children of a match can also be a match, but I'm allowing a parameter to control for this behavior

--- test_viewtree_search.py
+++ test_viewtree_search.py
@@ -32,17 +32,30 @@ class TestCases(TestCase):
             {'case_name': 'test_case5',
              'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json',
              'case_search_commands': [".container"],
-             'case_count': 1
+             'case_count': 6
              },
-            {'case_name': 'test_case6',
+            {'case_name': 'a simple selector finds a match then does not explore inside its children',
              'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json',
-             'case_search_commands': ['CvarSelect', '#rate'],
-             'case_count': 1
-             }
+             'case_search_commands': ['StackView'],
+             'case_count': 6
+             },
+
+            {'case_name': 'embedded in wrong parent type -- label',
+             'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\mos_eisley.json',
+             'case_search_commands': ['CvarCheckbox'],
+             'case_count': 0
+             },
+             {'case_name': 'a complete compound match contains another complete match-- the second is disregarded',
+              'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\mos_eisley.json',
+              'case_search_commands': ['#System', '.container'],
+              'case_count': 2
+              }
         ]
         f = io.StringIO()
         with redirect_stdout(f):
             for test_case in test_cases:
                 with open(test_case['case_file'], 'r') as f1:
                     z = load(f1)
-                self.assertEqual(viewtree_search(z,test_case['case_search_commands'] ), test_case['case_count'])
+                test_hits = [0]*len(test_case['case_search_commands'])
+                self.assertEqual(viewtree_search(z,test_case['case_search_commands'], test_hits, halt_on_match=False ), test_case['case_count'])
+
