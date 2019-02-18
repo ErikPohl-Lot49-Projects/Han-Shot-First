9b053a3f1230d53126b7ac6130858feef22fb3cb
Erik Pohl <iotasubscript@gmail.com>
2019-02-18 10:52:26 -0500

    cleaned up the code
    maintained pep8 linting

--- viewtree_search_demo_usage.py
+++ viewtree_search_demo_usage.py
@@ -11,20 +11,7 @@ def output_results(results):
     print('{} results found'.format(len(results)))
 
 
-def simple_search(
-        demo_search_object,
-        search
-):
-    search_hits = [0]*len(search)
-    search_results = demo_search_object.viewtree_search(
-            viewtree_search_object.json_source,
-            search,
-            search_hits
-        )
-    output_results(search_results)
-
-
-def search_halt_on_match_parm(
+def perform_search(
         demo_search_object,
         search,
         halt_on_match
@@ -42,28 +29,28 @@ viewtree_search_object = viewtree_search_cli()
 with open(modal_nodes_test_file, 'r') as test_file_handle:
     viewtree_search_object.json_source = load(test_file_handle)
 
-simple_search(viewtree_search_object, ['Input'])
-simple_search(viewtree_search_object, ['#videoMode'])
-simple_search(viewtree_search_object, ['#rate'])
-simple_search(viewtree_search_object, ['#supersample'])
-search_halt_on_match_parm(viewtree_search_object, ['.container'], True)
+perform_search(viewtree_search_object, ['Input'], False)
+perform_search(viewtree_search_object, ['#videoMode'], False)
+perform_search(viewtree_search_object, ['#rate'], False)
+perform_search(viewtree_search_object, ['#supersample'], False)
+perform_search(viewtree_search_object, ['.container'], False)
 
 with open(mos_eisley_test_file, 'r') as test_file_handle:
     viewtree_search_object.json_source = load(test_file_handle)
-search_halt_on_match_parm(viewtree_search_object, ['#System'], True)
-search_halt_on_match_parm(
+perform_search(viewtree_search_object, ['#System'], True)
+perform_search(
     viewtree_search_object,
     ['#System', '.container'],
     True
 )
-search_halt_on_match_parm(
+perform_search(
     viewtree_search_object,
     ['#System', '.container'],
     False
 )
-search_halt_on_match_parm(
+perform_search(
     viewtree_search_object,
     ['StackView', '.container'],
     False
 )
-search_halt_on_match_parm(viewtree_search_object, ['CvarCheckbox'], False)
+perform_search(viewtree_search_object, ['CvarCheckbox'], False)
