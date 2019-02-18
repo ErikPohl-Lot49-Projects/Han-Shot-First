07f8b75fdf12eb9c7b1b7e32ee7fb8ecf24f1a3e
Erik Pohl <iotasubscript@gmail.com>
2019-02-18 12:32:58 -0500

    added docsys
    and refactored

--- viewtree_search_demo_usage.py
+++ viewtree_search_demo_usage.py
@@ -6,9 +6,11 @@ mos_eisley_test_file = 'mos_eisley.json'
 
 
 def output_results(results):
+    '''Output viewtree search results'''
     for result in results:
         print(result)
     print('{} results found'.format(len(results)))
+    return True
 
 
 def perform_search(
@@ -16,6 +18,7 @@ def perform_search(
         search,
         halt_on_match
 ):
+    '''This is a wrapper for the viewtree search method call'''
     search_hits = [0]*len(search)
     search_results = demo_search_object.viewtree_search(
             viewtree_search_object.json_source,
@@ -24,33 +27,39 @@ def perform_search(
             halt_on_match=halt_on_match
         )
     output_results(search_results)
+    return True
 
-viewtree_search_object = viewtree_search_cli()
-with open(modal_nodes_test_file, 'r') as test_file_handle:
-    viewtree_search_object.json_source = load(test_file_handle)
+if __name__ == "__main__":
+    ''' Main demo usage functionality'''
 
-perform_search(viewtree_search_object, ['Input'], False)
-perform_search(viewtree_search_object, ['#videoMode'], False)
-perform_search(viewtree_search_object, ['#rate'], False)
-perform_search(viewtree_search_object, ['#supersample'], False)
-perform_search(viewtree_search_object, ['.container'], False)
+    '''Create an object for use in searching'''
+    viewtree_search_object = viewtree_search_cli()
 
-with open(mos_eisley_test_file, 'r') as test_file_handle:
-    viewtree_search_object.json_source = load(test_file_handle)
-perform_search(viewtree_search_object, ['#System'], True)
-perform_search(
-    viewtree_search_object,
-    ['#System', '.container'],
-    True
-)
-perform_search(
-    viewtree_search_object,
-    ['#System', '.container'],
-    False
-)
-perform_search(
-    viewtree_search_object,
-    ['StackView', '.container'],
-    False
-)
-perform_search(viewtree_search_object, ['CvarCheckbox'], False)
+    with open(modal_nodes_test_file, 'r') as test_file_handle:
+        viewtree_search_object.json_source = load(test_file_handle)
+
+    perform_search(viewtree_search_object, ['Input'], False)
+    perform_search(viewtree_search_object, ['#videoMode'], False)
+    perform_search(viewtree_search_object, ['#rate'], False)
+    perform_search(viewtree_search_object, ['#supersample'], False)
+    perform_search(viewtree_search_object, ['.container'], False)
+
+    with open(mos_eisley_test_file, 'r') as test_file_handle:
+        viewtree_search_object.json_source = load(test_file_handle)
+    perform_search(viewtree_search_object, ['#System'], True)
+    perform_search(
+        viewtree_search_object,
+        ['#System', '.container'],
+        True
+    )
+    perform_search(
+        viewtree_search_object,
+        ['#System', '.container'],
+        False
+    )
+    perform_search(
+        viewtree_search_object,
+        ['StackView', '.container'],
+        False
+    )
+    perform_search(viewtree_search_object, ['CvarCheckbox'], False)
