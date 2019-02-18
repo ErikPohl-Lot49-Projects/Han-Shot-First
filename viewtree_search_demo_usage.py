4fe69027cfa74a6bacf72adb2af44cdf7c44b43a
Erik Pohl <iotasubscript@gmail.com>
2019-02-18 10:45:44 -0500

    pep8 linted the code
    pep8 linted the code

--- viewtree_search_demo_usage.py
+++ viewtree_search_demo_usage.py
@@ -1,33 +1,69 @@
 from json import load
-from viewtree_search import viewtree_search
+from viewtree_search_cli import viewtree_search_cli
 
-with open('C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json','r') as f1:
-    z = load(f1)
-
-search_hits = [0]
-mysearch = ['Input']
-output = viewtree_search(z,mysearch , search_hits)
-print(output)
-mysearch = ['#videoMode']
-output = viewtree_search(z,mysearch , search_hits )
-print(output)
-mysearch = ['#rate']
-output = viewtree_search(z,mysearch , search_hits )
-print(output)
-mysearch = ['#supersample']
-output = viewtree_search(z,mysearch , search_hits )
-print(output)
-mysearch = ['.container']
-output = viewtree_search(z,mysearch , search_hits, halt_on_match=True )
-print(output)
+modal_nodes_test_file = 'the_modal_nodes.json'
+mos_eisley_test_file = 'mos_eisley.json'
 
 
-with open('C:\\Users\\Richard Pendrake\\Downloads\\mos_eisley.json','r') as f1:
-    z = load(f1)
-search_hits = [0,0]
-mysearch = ['#System', '.container']
-output = viewtree_search(z,mysearch , search_hits, halt_on_match=True )
-print(output)
-mysearch = ['#System', '.container']
-output = viewtree_search(z,mysearch , search_hits, halt_on_match=False)
-print(output)
+def output_results(results):
+    for result in results:
+        print(result)
+    print('{} results found'.format(len(results)))
+
+
+def simple_search(
+        demo_search_object,
+        search
+):
+    search_hits = [0]*len(search)
+    search_results = demo_search_object.viewtree_search(
+            viewtree_search_object.json_source,
+            search,
+            search_hits
+        )
+    output_results(search_results)
+
+
+def search_halt_on_match_parm(
+        demo_search_object,
+        search,
+        halt_on_match
+):
+    search_hits = [0]*len(search)
+    search_results = demo_search_object.viewtree_search(
+            viewtree_search_object.json_source,
+            search,
+            search_hits,
+            halt_on_match=halt_on_match
+        )
+    output_results(search_results)
+
+viewtree_search_object = viewtree_search_cli()
+with open(modal_nodes_test_file, 'r') as test_file_handle:
+    viewtree_search_object.json_source = load(test_file_handle)
+
+simple_search(viewtree_search_object, ['Input'])
+simple_search(viewtree_search_object, ['#videoMode'])
+simple_search(viewtree_search_object, ['#rate'])
+simple_search(viewtree_search_object, ['#supersample'])
+search_halt_on_match_parm(viewtree_search_object, ['.container'], True)
+
+with open(mos_eisley_test_file, 'r') as test_file_handle:
+    viewtree_search_object.json_source = load(test_file_handle)
+search_halt_on_match_parm(viewtree_search_object, ['#System'], True)
+search_halt_on_match_parm(
+    viewtree_search_object,
+    ['#System', '.container'],
+    True
+)
+search_halt_on_match_parm(
+    viewtree_search_object,
+    ['#System', '.container'],
+    False
+)
+search_halt_on_match_parm(
+    viewtree_search_object,
+    ['StackView', '.container'],
+    False
+)
+search_halt_on_match_parm(viewtree_search_object, ['CvarCheckbox'], False)
