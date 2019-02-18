461d5ef80b64e567d61a12c469fb047417dfcd25
Erik Pohl <iotasubscript@gmail.com>
2019-02-18 10:24:29 -0500

    pep8 linted the code
    pep8 linted the code

--- test_viewtree_search.py
+++ test_viewtree_search.py
@@ -9,66 +9,91 @@ import io
 class TestCases(TestCase):
 
     def test_batches_of_cases(self):
+        self.modal_nodes_test_file = 'the_modal_nodes.json'
+        self.mos_eisley_test_file = 'mos_eisley.json'
         test_cases = [
-            {'case_name' : 'test_case1',
-             'case_file' :  'C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json',
-             'case_search_commands' : ["Input"],
-             'case_count' : 26
-             },
-            {'case_name': 'test_case2',
-             'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json',
-             'case_search_commands': ["#videoMode"],
-             'case_count': 1
-             },
-            {'case_name': 'test_case3',
-             'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json',
-             'case_search_commands': ["#rate"],
-             'case_count': 1
-             },
-            {'case_name': 'test_case4',
-             'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json',
-             'case_search_commands': ["#supersample"],
-             'case_count': 1
-             },
-            {'case_name': 'test_case5',
-             'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json',
-             'case_search_commands': [".container"],
-             'case_count': 6
-             },
-            {'case_name': 'a simple selector finds a match then does not explore inside its children',
-             'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\the_modal_nodes.json',
-             'case_search_commands': ['StackView'],
-             'case_count': 6
-             },
-            {'case_name': 'embedded in wrong parent type -- label',
-             'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\mos_eisley.json',
-             'case_search_commands': ['CvarCheckbox'],
-             'case_count': 0
-             },
-
-             {'case_name': 'a complete compound match contains another complete match-- the second is disregarded',
-              'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\mos_eisley.json',
-              'case_search_commands': ['#System', '.container'],
-              'case_count': 2
-              },
-            {'case_name': 'before combinator, compound',
-             'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\mos_eisley.json',
-             'case_search_commands': ['StackView', '.container'],
-             'case_count': 2
-             }
-            #,
-            #{'case_name': 'with combinator, complex/chain with descendent',
-            # 'case_file': 'C:\\Users\\Richard Pendrake\\Downloads\\mos_eisley.json',
-            # 'case_search_commands': ['StackView', ' ', '.container'],
-            # 'case_count': 2
-            # }
+            {
+                'case_name': 'test_case1',
+                'case_file':
+                    self.modal_nodes_test_file,
+                'case_search_commands': ["Input"],
+                'case_count': 26
+            },
+            {
+                'case_name': 'test_case2',
+                'case_file':
+                    self.modal_nodes_test_file,
+                'case_search_commands': ["#videoMode"],
+                'case_count': 1
+            },
+            {
+                'case_name': 'test_case3',
+                'case_file':
+                    self.modal_nodes_test_file,
+                'case_search_commands': ["#rate"],
+                'case_count': 1
+            },
+            {
+                'case_name': 'test_case4',
+                'case_file':
+                    self.modal_nodes_test_file,
+                'case_search_commands': ["#supersample"],
+                'case_count': 1
+            },
+            {
+                'case_name': 'test_case5',
+                'case_file':
+                    self.modal_nodes_test_file,
+                'case_search_commands': [".container"],
+                'case_count': 6
+            },
+            {
+                'case_name': 'a simple selector finds a match '
+                             'then does not explore inside its children',
+                'case_file':
+                    self.modal_nodes_test_file,
+                'case_search_commands': ['StackView'],
+                'case_count': 6
+            },
+            {
+                'case_name': 'embedded in wrong parent type -- label',
+                'case_file':
+                    self.mos_eisley_test_file,
+                'case_search_commands': ['CvarCheckbox'],
+                'case_count': 0
+            },
+            {
+                'case_name':
+                    'a complete compound match contains '
+                    'another complete match-- the second is disregarded',
+                'case_file':
+                    self.mos_eisley_test_file,
+                'case_search_commands': ['#System', '.container'],
+                'case_count': 2
+            },
+            {
+                'case_name': 'before combinator, compound',
+                'case_file':
+                    self.mos_eisley_test_file,
+                'case_search_commands': ['StackView', '.container'],
+                'case_count': 2
+            }
         ]
-        f = io.StringIO()
-        with redirect_stdout(f):
-            vts_cli = viewtree_search_cli()
+        output_redirect = io.StringIO()
+        with redirect_stdout(output_redirect):
+            viewtree_search_object = viewtree_search_cli()
             for test_case in test_cases:
-                with open(test_case['case_file'], 'r') as f1:
-                    vts_cli.json_source = load(f1)
+                with open(test_case['case_file'], 'r') as test_file_handle:
+                    viewtree_search_object.json_source = load(test_file_handle)
                 test_hits = [0]*len(test_case['case_search_commands'])
-                self.assertEqual(len(vts_cli.viewtree_search(vts_cli.json_source,test_case['case_search_commands'], test_hits, halt_on_match=False )), test_case['case_count'])
-
+                self.assertEqual(
+                    len(
+                        viewtree_search_object.viewtree_search(
+                            viewtree_search_object.json_source,
+                            test_case['case_search_commands'],
+                            test_hits,
+                            halt_on_match=False
+                        )
+                    ),
+                    test_case['case_count']
+                )
