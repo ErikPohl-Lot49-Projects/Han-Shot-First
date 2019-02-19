76b54abd73a940c37c701cd40b1334a3b2a59049
Erik Pohl <iotasubscript@gmail.com>
2019-02-19 10:30:43 -0500

    added comment headers
    added comment headers

--- viewtree_search_demo_usage.py
+++ viewtree_search_demo_usage.py
@@ -1,6 +1,19 @@
-from json import load
+#!/usr/bin/env python
+
+'''
+viewtree_search_demo_usage instantiates the viewtree_search_cli and executes demo searches
+'''
+
 from viewtree_search_cli import viewtree_search_cli
 
+__author__ = "Erik Pohl"
+__copyright__ = "None"
+__credits__ = ["Erik Pohl"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Erik Pohl"
+__email__ = "erik.pohl.444@gmail.com"
+__status__ = "Code Review"
 
 def perform_search(
         demo_search_object,
