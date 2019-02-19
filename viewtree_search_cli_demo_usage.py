fe6a5b36aba0f08317bd577c83f7f0d88a7556ff
Erik Pohl <iotasubscript@gmail.com>
2019-02-19 10:31:25 -0500

    added comment headers
    added comment headers

--- viewtree_search_cli_demo_usage.py
+++ viewtree_search_cli_demo_usage.py
@@ -1,4 +1,21 @@
+#!/usr/bin/env python
+
+
+'''
+viewtree_search_cli_demo_usage instantiates the viewtree_search_cli and executes the prompt
+'''
+
 from viewtree_search_cli import viewtree_search_cli
+
+__author__ = "Erik Pohl"
+__copyright__ = "None"
+__credits__ = ["Erik Pohl"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Erik Pohl"
+__email__ = "erik.pohl.444@gmail.com"
+__status__ = "Code Review"
+
 
 search_cli = viewtree_search_cli()
 search_cli.prompt()
