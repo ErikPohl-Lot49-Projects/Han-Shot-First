252d25fe62ebd02e0cbc57ed01785e92cdb1931d
Erik Pohl <iotasubscript@gmail.com>
2019-02-21 23:18:13 -0500

    pep8 linted recent changes
    fixed line length

--- viewtree_search_cli.py
+++ viewtree_search_cli.py
@@ -4,7 +4,7 @@
 Provides a CLI for a user to load JSON from files
 or URLs and then search it with selectors
 '''
-    
+
 import logging
 import sys
 import re
@@ -19,6 +19,7 @@ __version__ = "1.0.0"
 __maintainer__ = "Erik Pohl"
 __email__ = "erik.pohl.444@gmail.com"
 __status__ = "Code Review"
+
 
 class viewtree_search_cli:
     '''
@@ -56,7 +57,7 @@ class viewtree_search_cli:
 
     def _set_logging(self):
         '''
-        set logging on or off for info 
+        set logging on or off for info
         logging based on debug_mode
         method
         '''
@@ -79,7 +80,7 @@ class viewtree_search_cli:
         logging.info(set(string_command[1:]).intersection(self.delims))
         if set(string_command[1:]).intersection(self.delims):
             command_list = [''.join(prefix_pair) for prefix_pair in list(
-                    zip(split_list[1::2], split_list[2::2]))]
+                zip(split_list[1::2], split_list[2::2]))]
             if string_command[0] not in self.delims:
                 command_list = split_list[0:1] + command_list
         logging.info('command list' + str(command_list))
@@ -130,7 +131,7 @@ class viewtree_search_cli:
                         halt_on_match=halt_on_match,
                         debug_mode=debug_mode,
                         allow_double_matching=allow_double_matching,
-                        level=level+1
+                        level=level + 1
                     )
                 )
             return results
@@ -140,7 +141,7 @@ class viewtree_search_cli:
         '''
         if isinstance(json_view_tree, dict):
             logging.info('found a dictionary.  searching it.')
-            match=False
+            match = False
             for current_json_key in json_view_tree:
                 '''
                 let's see if the value of the json_key
@@ -164,7 +165,7 @@ class viewtree_search_cli:
                             halt_on_match=halt_on_match,
                             debug_mode=debug_mode,
                             allow_double_matching=allow_double_matching,
-                            level=level+1
+                            level=level + 1
                         )
                     )
                 else:
@@ -180,13 +181,15 @@ class viewtree_search_cli:
                             if (
                                     (
                                             command.startswith('#') and
-                                            (current_json_key == 'identifier') and
+                                            (current_json_key ==
+                                             'identifier') and
                                             (current_json_value ==
                                              command[1:])
                                     ) or
                                     (
                                             command.startswith('.') and
-                                            (current_json_key == 'classNames') and
+                                            (current_json_key ==
+                                             'classNames') and
                                             (command[1:] in
                                              current_json_value)
                                     ) or
@@ -199,7 +202,9 @@ class viewtree_search_cli:
                                     )
                             ):
                                 local_search_hits[command_index] += 1
-                                if self.check_full_search_match(local_search_hits):
+                                if self.check_full_search_match(
+                                        local_search_hits
+                                ):
                                     result = json_view_tree
                                     match = True
                                     results.append(result)
@@ -213,18 +218,18 @@ class viewtree_search_cli:
         the search results
         '''
         for finding_number, result in enumerate(results):
-            print('Finding {}'.format(finding_number+1))
+            print('Finding {}'.format(finding_number + 1))
             print('-' * 80)
             print(dumps(result, indent=4))
-            print('-'*80)
+            print('-' * 80)
         print("Found 1 entry") \
-            if len(results) == 1\
+            if len(results) == 1 \
             else print("Found {} entries".format(len(results)))
         return len(results)
 
     def viewtree_search_wrapper(self, command_string):
         command_list = self._split_string_command(command_string)
-        command_hits = len(command_list)*[0]
+        command_hits = len(command_list) * [0]
         search_results = self.viewtree_search(
             self.json_source,
             command_list,
@@ -357,7 +362,7 @@ class viewtree_search_cli:
                       "You will never find a more wretched hive "
                       "of scum and villainy. We must be cautious.")
             elif command_string.lower() == 'source':
-                print(dumps(self.json_source, indent=4))\
+                print(dumps(self.json_source, indent=4)) \
                     if self.json_source \
                     else print("No JSON source has been loaded "
                                "for viewtree searching")
